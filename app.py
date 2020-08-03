ea83e3c0214f7f6b33b5c79bf6bd0109415fa259
Jonathan Wong <jonathan.a.wong@gmail.com>
2020-08-03 12:17:57 -0700

    added routes to app.py and created the Cupcake class

--- app.py
+++ app.py
@@ -1,1 +1,60 @@
 """Flask app for Cupcakes"""
+
+
+from flask import Flask, request, redirect, render_template, flash, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/api/cupcakes")
+def list_all_cupcakes():
+
+    cupcakes = Cupcake.query.all()
+    serialized = [cupcake.serialize() for cupcake in cupcakes]
+    # For each instance of cupcake, we serialize(turn into a dict) each instance of cupcake. 
+    # TODO: Ask why we pass cupcake in to the serialize method up here and not in the single method
+
+    return jsonify(cupcakes=serialized)
+
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def list_single_cupcake(cupcake_id):
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.route("/api/cupcakes", methods=['POST'])
+def create_cupcake():
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
+
+
+
+
+    
+
